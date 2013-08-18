3c0e6a87d58291eddba13b021de5555ec115ff82
Taian Su <taiansu@gmail.com>
2013-08-18 11:07:20 +0800

    init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CashBook::Application.config.secret_key_base = 'a3694d18f710074400eda4f4759047b4fdfadee8b4b4cdeb004e551ac3b8aebbfe7c72def98f058a0dbd404363fbc47c9edd41d27e76e1fcad069b88d6b58336'
