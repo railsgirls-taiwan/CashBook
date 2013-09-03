a54320410351d804317420bf38cd3bcbfe3e5672
簡煒航 <tonytonyjan@gmail.com>
2013-09-03 20:06:27 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ CashBook::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'record#index'
+  root 'records#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
